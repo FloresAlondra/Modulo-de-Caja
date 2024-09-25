75b6ebc4632f922caffbdbb078c18da66505b98a
Alondra Espinoza <alondraespinoza897@gmail.com>
2024-09-24 19:48:26 -0600

    Inicio del Modulo de caja

---
+++ Modulo de Caja/ModulodeCaja.h
@@ -0,0 +1,17 @@
+#ifndef MODULODECAJA_H
+#define MODULODECAJA_H
+
+typedef class nodo
+{
+	public:
+		void insertarCajero();
+		void mostrarCajeros();
+		void modificarCajeros();
+		void eliminarCajero();
+		int cajero;
+
+
+
+}node;
+
+#endif // !MODULODECAJA_H
