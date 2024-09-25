0935198e1af66f1acb3f82a5b5ff6dc2a0306646
Alondra Espinoza <alondraespinoza897@gmail.com>
2024-09-24 19:49:56 -0600

    Actualizacion de Modulo de cajero

--- Modulo de Caja/Modulo de Caja.cpp
+++ Modulo de Caja/Modulo de Caja.cpp
@@ -4,7 +4,17 @@
 
 using namespace std;
 
-
+int menu()
+{
+	cout << "-_-_-_-_-_-Bienvenido al menu de Cajeros-_-_-_-_-_-" << endl;
+	cout << "1)Para agregar cajeros" << endl;
+	cout << "2)Para Mostrar cajeros" << endl;
+	cout << "3)Para Eliminar cajeros" << endl;
+	cout << "4)Para Modificar cajeros" << endl;
+	cout << "5)Para volver al menu principal" << endl;
+	int opcion;
+	cin >> opcion;
+}
 
 int main()
 {
@@ -19,28 +29,10 @@ int main()
 		{
 			case 1:
 
-
-
-
-
-
-
-
-
 		}
 
 	} while (opcion != 7);
 
 }
 
-int menu()
-{
-	cout << "-_-_-_-_-_-Bienvenido al menu de Cajeros-_-_-_-_-_-" << endl;
-	cout << "1)Para agregar cajeros" << endl;
-	cout << "2)Para Mostrar cajeros" << endl;
-	cout << "3)Para Eliminar cajeros" << endl;
-	cout << "4)Para Modificar cajeros" << endl;
-	cout << "5)Para volver al menu principal" << endl;
-	int opcion;
-	cin >> opcion;
-}
+
